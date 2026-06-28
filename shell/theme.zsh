03a69e38f16b45cc364d2594ad2a42c4eaad5e81
wrench <amichelangelo@outlook.com>
2026-06-29 01:19:56 +0800

    feat(prompt):add root prompt theme

--- shell/theme.zsh
+++ shell/theme.zsh
@@ -5,29 +5,46 @@ autoload -Uz colors && colors
 TOP_LEFT_CHAR=${TOP_LEFT_CHAR:-'╭'}  
 BOTTOM_LEFT_CHAR=${BOTTOM_LEFT_CHAR:-'╰'} 
 HORIZ=${HORIZ:-'─'}                   
+ROOT_PROMPT_CHAR=${ROOT_PROMPT_CHAR:-'#'}
 PROMPT_CHAR=${PROMPT_CHAR:-'$'} 
 
+ROOT_USER_COLOR=${ROOT_USER_COLOR:-red}
 USER_COLOR=${USER_COLOR:-cyan}
 HOST_COLOR=${HOST_COLOR:-magenta}
 PWD_COLOR=${PWD_COLOR:-blue}
+ROOT_PROMPT_SYMBOL_COLOR=${PROMPT_SYMBOL_COLOR:-red}
 PROMPT_SYMBOL_COLOR=${PROMPT_SYMBOL_COLOR:-cyan}
-SUDO_COLOR=${SUDO_COLOR:-blue}
 
+
+ROOT_FIRST_LINE() {
+	printf "%s%s(%s%s%s%s%s%s)-[%s%s%s]" \
+		"$TOP_LEFT_CHAR" "$HORIZ" \
+		"%F{$ROOT_USER_COLOR}" "%n" "%f" \
+		"%F{$HOST_COLOR}" "@%m" "%f" \
+		"%F{$PWD_COLOR}" "%~" "%f"
+}
 
 FIRST_LINE() {
-  printf "%s%s(%s%s%s%s%s%s)-[%s%s%s]" \
-    "$TOP_LEFT_CHAR" "$HORIZ" \
-    "%F{$USER_COLOR}" "%n" "%f" \
-    "%F{$HOST_COLOR}" "@%m" "%f" \
-    "%F{$PWD_COLOR}" "%~" "%f"
+	printf "%s%s(%s%s%s%s%s%s)-[%s%s%s]" \
+		"$TOP_LEFT_CHAR" "$HORIZ" \
+		"%F{$USER_COLOR}" "%n" "%f" \
+		"%F{$HOST_COLOR}" "@%m" "%f" \
+		"%F{$PWD_COLOR}" "%~" "%f"
 }
 
 SECOND_PREFIX() {
-  printf "%s%s" "$BOTTOM_LEFT_CHAR" "$HORIZ"
+	printf "%s%s" "$BOTTOM_LEFT_CHAR" "$HORIZ"
 }
 
-PROMPT='$(FIRST_LINE)'            
-PROMPT+='%(?..%F{red} ✖ %? %f)'
-PROMPT+=$'\n'                    
-PROMPT+='$(SECOND_PREFIX)%F{'"$PROMPT_SYMBOL_COLOR"'}'"$PROMPT_CHAR"'%f '  
 
+if [[ $EUID -eq 0 ]]; then
+	PROMPT='$(ROOT_FIRST_LINE)'            
+	PROMPT+='%(?..%F{red} ✖ %? %f)'
+	PROMPT+=$'\n'                    
+	PROMPT+='$(SECOND_PREFIX)%F{'"$ROOT_PROMPT_SYMBOL_COLOR"'}'"$ROOT_PROMPT_CHAR"'%f '  
+else
+	PROMPT='$(FIRST_LINE)'            
+	PROMPT+='%(?..%F{red} ✖ %? %f)'
+	PROMPT+=$'\n'                    
+	PROMPT+='$(SECOND_PREFIX)%F{'"$PROMPT_SYMBOL_COLOR"'}'"$PROMPT_CHAR"'%f '  
+fi
